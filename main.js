994f5d1c537faa7d2204867ec46f37d1b0a4c572
linker <llin@junglescout.com>
2021-10-11 15:02:02 +0800

    [+] generate log new method

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 console.log('Hello World, semantic-release')
 console.log('Use semantic-release build version')
 console.log('try build next release')
+console.log('run auto-changelog update changelog')
