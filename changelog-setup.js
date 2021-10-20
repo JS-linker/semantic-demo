60849c93eace89078a04f64c056ad788c902fa57
linker <llin@junglescout.com>
2021-10-20 18:08:10 +0800

    [+] remove log

--- changelog-setup.js
+++ changelog-setup.js
@@ -1,6 +1,5 @@
 module.exports = function (Handlebars) {
   Handlebars.registerHelper('custom', function (context, options) {
-    console.log('registerHelper -> context', context, options)
     return 'custom helpers!'
   })
 }
